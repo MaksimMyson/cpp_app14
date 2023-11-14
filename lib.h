7c114ed8b7a36ed6f6051a39ac35e0042ea81266
MaksimMyson <maksimmyson33@gmail.com>
2023-11-14 22:19:28 +0200

    lb

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+#include <iostream>
 
+using namespace std;
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SQUARE(x) ((x) * (x))
+#define POWER(x, n) (pow((x), (n)))
+#define IS_EVEN(x) ((x) % 2 == 0)
+#define IS_ODD(x) ((x) % 2 != 0)
---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+
+#include <cmath>
+#include "lib.h"
+
+int main() {
+    int num1, num2;
+
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+
+    cout << "Min: " << MIN(num1, num2) << endl;
+    cout << "Max: " << MAX(num1, num2) << endl;
+    cout << "Square of num1: " << SQUARE(num1) << endl;
+    cout << "Enter a power: ";
+
+    int power;
+    cin >> power;
+    cout << num1 << " raised to the power " << power << ": " << POWER(num1, power) << endl;
+
+    cout << num1 << " is " << (IS_EVEN(num1) ? "even" : "odd") << endl;
+    cout << num2 << " is " << (IS_ODD(num2) ? "odd" : "even") << endl;
+
+    return 0;
+}
